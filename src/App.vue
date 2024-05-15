4515e4d7c4bf3fdd01c8eb8d9466aa578128c274
wu <66793170+pat2304107@users.noreply.github.com>
2024-05-15 11:48:23 +0800

    feat: 🎸 adjust main page style

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,34 @@ import Header from '@/components/Header/Header.vue';
         id="app"
         class="app"
     >
-        <Header />
-        <router-view />
+        <div class="main-container">
+            <Header />
+            <router-view />
+        </div>
     </div>
 </template>
 
 <style lang="scss">
 @import "./styles/index";
+
+.app {
+    position: relative;
+    overflow: hidden;
+    min-width: 480px;
+    min-height: 100vh;
+
+    .main-container {
+        position: relative;
+        overflow: hidden;
+        width: 100%;
+        z-index: 2;
+        height: 100vh;
+
+        @media (min-width: 576px) {
+            margin: 0 auto;
+            width: 576px;
+        }
+    }
+}
+
 </style>
